03ec762fde7570b3a187dbd923c2efd0a29f47b4
guest <chemistrycreative@ChemistrysMini2.localdomain>
2025-06-12 12:46:02 -0400

    feat: Add password-protected access to settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { SettingsProvider } from './context/SettingsContext';
 import Home from './pages/Home';
 import VideoBooth from './pages/VideoBooth';
@@ -7,19 +7,42 @@ import InfoPage from './pages/InfoPage';
 import SettingsPage from './pages/SettingsPage';
 import './App.css'
 
+// This component will contain the main app logic to use hooks
+const AppContent = () => {
+  const navigate = useNavigate();
+
+  const handleSecretButtonClick = () => {
+    const password = prompt('Enter password to access settings:');
+    if (password === 'chemlab305') {
+      navigate('/settings');
+    } else if (password !== null) {
+      alert('Incorrect password.');
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <div 
+        className="secret-settings-button"
+        onClick={handleSecretButtonClick}
+        title="Access Settings"
+      ></div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video" element={<VideoBooth />} />
+        <Route path="/photo-strip" element={<PhotoStrip />} />
+        <Route path="/info" element={<InfoPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </div>
+  );
+};
+
 function App() {
   return (
     <SettingsProvider>
       <Router>
-        <div className="min-h-screen bg-gray-900">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/video" element={<VideoBooth />} />
-            <Route path="/photo-strip" element={<PhotoStrip />} />
-            <Route path="/info" element={<InfoPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-          </Routes>
-        </div>
+        <AppContent />
       </Router>
     </SettingsProvider>
   );
